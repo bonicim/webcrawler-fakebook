0c2ecfdd3f4392c1962b37d2602d21079a0e07f7
Mark Bonicillo <markabonicillo@gmail.com>
2017-11-29 19:01:33 -0800

    Added stubs.

---
+++ webcrawler.py
@@ -0,0 +1,30 @@
+import socket
+import urllib.request
+import urllib.parse
+import html.parser
+import http.cookies
+import http.cookiejar
+import time
+import htmlparser
+import threading
+import sys
+
+
+def getopts(argv):
+    # TODO
+    pass
+
+
+def main():
+    lock = threading.Lock
+    list_frontier = []
+    list_flags = []
+    set_users = set()
+    my_args = getopts(sys.argv)
+
+
+if __name__ =="__main":
+    main()
+
+
+main()
