4b0189902a01d7f958d74359e74e30c4ff53648f
Mark Bonicillo <markabonicillo@gmail.com>
2017-11-30 16:30:03 -0800

    Passed getopts tests. 13 test skipped.

--- test_webcrawler.py
+++ test_webcrawler.py
@@ -1,5 +1,6 @@
 import unittest
 import webcrawler
+import pytest
 
 
 class WebcrawlerTestCase(unittest.TestCase):
@@ -11,70 +12,71 @@ class WebcrawlerTestCase(unittest.TestCase):
     REL_FRIEND_URL = ''
     FRIEND_URL = ''
     FRIEND_PAGE_URL = ''
+    ARGV = ['webcrawler', USERNAME, PASSWORD]
 
-    # TODO Write tests
     def test_getopts_returns_username_password(self):
-        """Is a diciontary containing username and password retrieved?"""
-        pass
+        """Is a list containing username and password retrieved?"""
+        assert webcrawler.getopts(WebcrawlerTestCase.ARGV) == ['bonicillo.m', 'toast']
 
     def test_getopts_returns_error(self):
         """Does error get returned if 0, 1, or 3 more arguments are given?"""
-        pass
+        with self.assertRaises(SystemExit):
+            webcrawler.getopts(['toast'])
 
     def test_login_fakebook_success(self):
         """Does a valid user's fakebook profile get returned?"""
-        pass
+        pytest.skip()
 
     def test_login_fakebook_failure(self):
         """Does an error page for an invalid fakebook profile get returned?"""
-        pass
+        pytest.skip()
 
     def test_open_friend_page_success(self):
         """Does a valid fakebook landing page of a friend get returned?"""
-        pass
+        pytest.skip()
 
     def test_open_friend_page_failure(self):
         """Does an error page for an invalid fakebook profile get returned?"""
-        pass
+        pytest.skip()
 
     def test_open_view_friends_page_success(self):
         """Does a fakebook view friends page of a valid friend get returned?"""
-        pass
+        pytest.skip()
 
     def test_open_view_friends_page_failure(self):
         """Does an error page for an invalid view friends url get returned?"""
-        pass
+        pytest.skip()
 
     def test_parse_flags_friends_nextpage_success(self):
         """Does a dictionary containing 3 key value pairs get returned?"""
-        pass
+        pytest.skip()
 
     def test_parse_flags_friends_nextpage_no_next(self):
         """Does a dictionary containing 3 key value pairs in which the next_page key has
         an empty list get returned?"""
-        pass
+        pytest.skip()
 
     def test_parse_flags_friends_nextpage_no_flag(self):
         """Does a dictionary containing 3 key value pairs in which the flags key has
         an empty list get returned?"""
-        pass
+        pytest.skip()
 
     def test_parse_flags_friends_nextpage_no_friends(self):
         """Does a dictionary containing 3 key value pairs in which the friends key has
         an empty list get returned?"""
-        pass
+        pytest.skip()
 
     def test_create_get_req_friend_url_success(self):
         """Is a Request object created given a valid url?"""
-        pass
+        pytest.skip()
 
     def test_create_post_req_fb_login_success(self):
         """Is a Post object created given a valid url?"""
-        pass
+        pytest.skip()
 
     def test_create_fb_absolute_url_success(self):
         """Is an absolute FB url created given a relative friend url?"""
-        pass
+        pytest.skip()
 
 
 if __name__ == '__main__':
