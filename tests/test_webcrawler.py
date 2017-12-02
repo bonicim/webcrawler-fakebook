52dad3c1af19649fdccebe169be059c2f481475f
Mark Bonicillo <markabonicillo@gmail.com>
2017-12-01 17:03:45 -0800

    Cleaned up tests.

--- tests/test_webcrawler.py
+++ tests/test_webcrawler.py
@@ -1,8 +1,9 @@
 import unittest
 import pytest
 import src.webcrawler
-import tests.test_const
+import tests.util_test_const
 import urllib.request
+import tests.util_test_html
 
 
 class WebcrawlerTestCase(unittest.TestCase):
@@ -11,7 +12,7 @@ class WebcrawlerTestCase(unittest.TestCase):
 
     def test_getopts_returns_username_password(self):
         """Is a list containing username and password retrieved?"""
-        assert src.webcrawler.getopts(tests.test_const.ARGV) == ['bonicillo.m', 'toast']
+        assert src.webcrawler.getopts(tests.util_test_const.ARGV) == ['bonicillo.m', 'toast']
 
     def test_getopts_returns_error(self):
         """Does error get returned if 0, 1, or 3 more arguments are given?"""
@@ -45,39 +46,39 @@ class WebcrawlerTestCase(unittest.TestCase):
     def test_parse_flags_friends_my_fb_page(self):
         """Does a dictionary containing 3 key value pairs in which the next_page key has
         an empty list get returned?"""
-        dict_ret = src.webcrawler.parse_flags_friends_nextpage(tests.test_const.MEMBER_LP_HTML)
+        dict_ret = src.webcrawler.parse_flags_friends_nextpage(tests.util_test_html.MEMBER_LANDING_HTML)
         assert len(dict_ret['next_page']) == 0
         assert len(dict_ret['friend']) == 10
         assert len(dict_ret['flag']) == 0
 
     def test_parse_flags_friends_nextpage_friend(self):
         """Does a dictionary containing 3 key value pairs get returned?"""
-        dict_ret = src.webcrawler.parse_flags_friends_nextpage(tests.test_const.FRIEND_LP_HTML)
+        dict_ret = src.webcrawler.parse_flags_friends_nextpage(tests.util_test_const.FRIEND_LP_HTML)
         for list_el in dict_ret:
             assert len(list_el) == 0
 
     def test_parse_flags_friends_viewing_friends_html(self):
         """Does a dictionary containing 3 key value pairs in which the flags key has
         an empty list get returned?"""
-        dict_ret = src.webcrawler.parse_flags_friends_nextpage(tests.test_const.VIEWING_FRIENDS_HTML)
+        dict_ret = src.webcrawler.parse_flags_friends_nextpage(tests.util_test_const.VIEWING_FRIENDS_HTML)
         assert len(dict_ret['flag']) == 0
         assert len(dict_ret['friend']) == 20
         assert len(dict_ret['flag']) == 0
 
     def test_create_get_req_friend_url_success(self):
         """Is a GET Request object created given a valid url?"""
-        self.assertIsInstance(src.webcrawler.create_get_req(tests.test_const.FRIEND_HOME_URL),
+        self.assertIsInstance(src.webcrawler.create_get_req(tests.util_test_const.FRIEND_HOME_URL),
                               urllib.request.Request)
 
     def test_create_post_req_fb_login_success(self):
         """Is a POST Request object created given a valid url?"""
-        self.assertIsInstance(src.webcrawler.create_post_req(tests.test_const.FRIEND_HOME_URL, tests.test_const.LOGIN_DATA),
+        self.assertIsInstance(src.webcrawler.create_post_req(tests.util_test_const.FRIEND_HOME_URL, tests.util_test_const.LOGIN_DATA),
                               urllib.request.Request)
 
     def test_create_fb_absolute_url_success(self):
         """Is an absolute FB url created given a relative friend url?"""
         assert src.webcrawler.create_fb_absolute_url('/fakebook/50644342/') == \
-               tests.test_const.FB_URL_PREFIX + '/fakebook/50644342/'
+               tests.util_test_const.FB_URL_PREFIX + '/fakebook/50644342/'
 
 
 if __name__ == '__main__':
